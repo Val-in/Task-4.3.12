fb509011900bd8bd993a80a5642bb1ba34576fcb
Val-in <valeriia.vp1@yandex.ru>
2024-05-08 15:45:46 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Задание_4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+
+            int temp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+            }
+        }
+    }
